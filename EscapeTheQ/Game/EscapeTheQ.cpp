1f5256466b16ec823dfc9121171e834d92182f27
hard-coded <cozyncoze@gmail.com>
2020-02-04 11:44:54 -0500

    add physics system

--- EscapeTheQ/Game/EscapeTheQ.cpp
+++ EscapeTheQ/Game/EscapeTheQ.cpp
@@ -1,5 +1,6 @@
 #include "EscapeTheQ.h"
 #include "Components/LuaScriptComponent.h"
+#include "EntitySystems/PhysicsSystem.h"
 #include "EntitySystems/LuaScriptSystem.h"
 #include "Input/InputEventSystem.h"
 #include "Mix/Entity.h"
@@ -19,7 +20,7 @@ bool EscapeTheQ::Init(unsigned long diskRequiredInMB, unsigned long memoryRequir
 	bool isInit = AwesomeEngineApp::Init(diskRequiredInMB, memoryRequiredInMB, virtualMemoryRequriedInMB, cpuSpeedRequiredInMHz);
 	if (isInit)
 	{
-		//mWorld.getSystemManager().addSystem<InputEventSystem>();
+		mWorld.getSystemManager().addSystem<PhysicsSystem>();
 		mWorld.getSystemManager().addSystem<LuaScriptSystem>();
 
 		// add lua component
@@ -33,7 +34,7 @@ void EscapeTheQ::Update(float dt)
 {
 	mWorld.update();
 	// TODO : polish entity component system
-	//mWorld.getSystemManager().getSystem<InputEventSystem>().Update(dt);
+	mWorld.getSystemManager().getSystem<PhysicsSystem>().Update(dt);
 	mWorld.getSystemManager().getSystem<LuaScriptSystem>().Update(dt);
 }
 
